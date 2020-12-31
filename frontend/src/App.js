7b811303e2b995a06560d030059cfae86514b0a9
Sahil Sharma <sahilkumar4544@gmail.com>
2021-01-01 00:00:46 +0530

    create home screen product list and rating componets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
-import context from 'react-bootstrap/esm/AccordionContext';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
     <Header />
     <main>
       <Container className = 'py-3'>
-      <h1>hello everyone</h1>
+      <HomeScreen/>
       </Container>
     </main>
     <Footer />
